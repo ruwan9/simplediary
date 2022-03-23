d650c42a3b6ece17ea840476c7e252c7b534e90d
ruwan9 <ruwan9@kakao.com>
2022-03-23 22:01:38 +0900

    feat: 사용자 입력 처리하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function DiaryEditor() {
-  return <div>DiaryEditor</div>;
+  const [state, setState] = useState({ author: "", content: "", emotion: 1 });
+
+  const handleChangeState = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input type="text" name="author" value={state.author} onChange={handleChangeState} />
+      </div>
+      <div>
+        <textarea name="content" value={state.content} onChange={handleChangeState}></textarea>
+      </div>
+      <div>
+        <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>저장</button>
+      </div>
+    </div>
+  );
 }
 
 export default DiaryEditor;
