2502f7203eabbb0b48b9fcb3c06106414ef4a09a
ruwan9 <ruwan9@kakao.com>
2022-04-11 15:09:05 +0900

    feat: useMemo 추가

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-// https://jsonplaceholder.typicode.com/comments
-
-const dummyList = [
-  {
-    id: 1,
-    author: 'Bob',
-    content: 'Bello',
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: 'Taylor',
-    content: 'Swift',
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: 'Louis',
-    content: 'Vuitton',
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: 'Bob',
+//     content: 'Bello',
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: 'Taylor',
+//     content: 'Swift',
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: 'Louis',
+//     content: 'Vuitton',
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
   const [data, setData] = useState([]); // 최종적으로 데이터를 관리할 state
@@ -87,10 +85,27 @@ function App() {
     getData();
   }, []);
 
+  // useMemo - return을 가진 함수들을 memoization을 통해 최적화시키기 위해 사용
+  // useMemo를 쓰는 순간부터 더이상 함수가 아니게 된다. (값을 return하기 때문)
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+    const goodCount = data.filter((it) => it.emotion >= 3).length; // data 중 emotion이 3 이상인 것들 추리기
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  // const { goodCount, badCount, goodRatio } = getDiaryAnalysis();
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <h1>일기장</h1>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
